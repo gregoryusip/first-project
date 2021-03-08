f4fdb2a2957494558c5be1f1e72a9a310caebf50
Gregoryus Imannuel Perdana <gregoryus@sirclo.com>
2021-03-08 15:37:31 +0700

    Update Handler

--- middleware/handlers.go
+++ middleware/handlers.go
@@ -1,294 +1,77 @@
-// LINK : https://codesource.io/build-a-crud-application-in-golang-with-postgresql/ & https://bambang-sso.medium.com/building-crud-api-using-golang-and-postgresql-469c3352f774
+// https://medium.com/@OmisNomis/creating-an-rpc-server-in-go-3a94797ab833 & https://medium.com/rungo/building-rpc-remote-procedure-call-network-in-go-5bfebe90f7e9
 
 package middleware
 
-import (
-	"database/sql"
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-	"strconv"
-
-	"github.com/gorilla/mux"
-
-	"github.com/gregoryusip/first-project/models"
-	"github.com/joho/godotenv"
-	_ "github.com/lib/pq"
-)
-
-type response struct {
-	ID      int64  `json:"id,omitempty"`
-	Message string `json:"message,omitempty"`
+type Product struct {
+	Name     string `json:"name"`
+	Price    int    `json:"price"`
+	Quantity int    `json:"quantity"`
 }
 
-func createConnection() *sql.DB {
-	err := godotenv.Load(".env")
-
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
-
-	db, err := sql.Open("postgres", os.Getenv("POSTGRES_URL"))
-
-	if err != nil {
-		panic(err)
-	}
-
-	err = db.Ping()
-
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Successfully Connected!")
-
-	return db
-
+type EditProduct struct {
+	Name        string `json:"name"`
+	NewName     string `json:"newname"`
+	NewPrice    int    `json:"price"`
+	NewQuantity int    `json:"quantity"`
 }
 
-func CreateProduct(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "POST")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+type Products int
 
-	var product models.Product
+var productSlice []Product
 
-	err := json.NewDecoder(r.Body).Decode(&product)
+func (p *Products) GetProduct(name string, reply *Product) error {
+	var found Product
 
-	if err != nil {
-		log.Fatalf("Unable to decode the request body. %v", err)
-	}
-
-	insertID := insertProduct(product)
-
-	res := response{
-		ID:      insertID,
-		Message: "Product created successfully",
-	}
-
-	json.NewEncoder(w).Encode(res)
-}
-
-func GetProduct(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-	params := mux.Vars(r)
-
-	id, err := strconv.Atoi(params["id"])
-
-	if err != nil {
-		log.Fatalf("Unable to convert the string into int. %v", err)
-	}
-
-	user, err := getProduct(int64(id))
-
-	if err != nil {
-		log.Fatalf("Unable to get usr. %v", err)
-	}
-
-	json.NewEncoder(w).Encode(user)
-}
-
-func GetAllProduct(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-	users, err := getAllProduct()
-
-	if err != nil {
-		log.Fatalf("Unable to get all user. %v", err)
-	}
-
-	json.NewEncoder(w).Encode(users)
-}
-
-func UpdateProduct(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "PUT")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
-	params := mux.Vars(r)
-
-	id, err := strconv.Atoi(params["id"])
-
-	if err != nil {
-		log.Fatalf("Unable to convert the string into int. %v", err)
-	}
-
-	var product models.Product
-
-	err = json.NewDecoder(r.Body).Decode(&product)
-
-	if err != nil {
-		log.Fatalf("Unable to decode the request body. %v", err)
-	}
-
-	updatedRows := updateProduct(int64(id), product)
-
-	msg := fmt.Sprintf("Product updated successfully. Total rows/record affected %v", updatedRows)
-
-	res := response{
-		ID:      int64(id),
-		Message: msg,
-	}
-
-	json.NewEncoder(w).Encode(res)
-}
-
-func DeleteProduct(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "DELETE")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
-	params := mux.Vars(r)
-
-	id, err := strconv.Atoi(params["id"])
-
-	if err != nil {
-		log.Fatalf("Unable to convert the string into int. %v", err)
-	}
-
-	deletedRows := deleteProduct(int64(id))
-
-	msg := fmt.Sprintf("Product deleted successfully. Total rows/record affected %v", deletedRows)
-
-	res := response{
-		ID:      int64(id),
-		Message: msg,
-	}
-
-	json.NewEncoder(w).Encode(res)
-}
-
-// 	HANDLER FUNCTION
-func insertProduct(product models.Product) int64 {
-	db := createConnection()
-
-	defer db.Close()
-
-	sqlStatement := `INSERT INTO products (name, price, quantity) VALUES ($1, $2, $3) RETURNING productid`
-
-	var id int64
-
-	err := db.QueryRow(sqlStatement, product.Name, product.Price, product.Quantity).Scan(&id)
-
-	if err != nil {
-		log.Fatalf("Unable to execute the query. %v", err)
-	}
-
-	fmt.Printf("Inserted a single record %v", id)
-
-	return id
-}
-
-func getProduct(id int64) (models.Product, error) {
-	db := createConnection()
-
-	defer db.Close()
-
-	var product models.Product
-
-	sqlStatement := `SELECT * FROM products WHERE productid=$1`
-
-	row := db.QueryRow(sqlStatement, id)
-
-	err := row.Scan(&product.ID, &product.Name, &product.Price, &product.Quantity)
-
-	switch err {
-	case sql.ErrNoRows:
-		fmt.Println("No rows were returned!")
-		return product, nil
-	case nil:
-		return product, nil
-	default:
-		log.Fatalf("Unable to scan the row. %v", err)
-	}
-
-	return product, err
-}
-
-func getAllProduct() ([]models.Product, error) {
-	db := createConnection()
-
-	defer db.Close()
-
-	var products []models.Product
-
-	sqlStatement := `SELECT * FROM products`
-
-	rows, err := db.Query(sqlStatement)
-
-	if err != nil {
-		log.Fatalf("Unable to execute the query. %v", err)
-	}
-
-	defer rows.Close()
-
-	for rows.Next() {
-		var product models.Product
-
-		err := rows.Scan(&product.ID, &product.Name, &product.Price, &product.Quantity)
-
-		if err != nil {
-			log.Fatalf("Unable to scan the row. %v", err)
+	for _, v := range productSlice {
+		if v.Name == name {
+			found = v
 		}
-
-		products = append(products, product)
 	}
 
-	return products, err
+	*reply = found
+
+	return nil
 }
 
-func updateProduct(id int64, product models.Product) int64 {
-	db := createConnection()
+func (p *Products) MakeProduct(product Product, reply *Product) error {
 
-	defer db.Close()
+	productSlice = append(productSlice, product)
 
-	sqlStatement := `UPDATE products SET name=$2, price=$3, quantity=$6 WHERE productid=$1`
+	*reply = product
 
-	res, err := db.Exec(sqlStatement, id, product.Name, product.Price, product.Quantity)
-
-	if err != nil {
-		log.Fatalf("Unable to execute the query. %v", err)
-	}
-
-	rowsAffected, err := res.RowsAffected()
-
-	if err != nil {
-		log.Fatalf("Error while checking the affected rows. %v", err)
-	}
-
-	fmt.Printf("Total rows/record affected %v", rowsAffected)
-
-	return rowsAffected
+	return nil
 }
 
-func deleteProduct(id int64) int64 {
-	db := createConnection()
+func (p *Products) EditProduct(product EditProduct, reply *Product) error {
 
-	defer db.Close()
+	var editedProduct Product
 
-	sqlStatement := `DELETE FROM products WHERE productid=$1`
+	for i, v := range productSlice {
+		if v.Name == product.Name {
+			productSlice[i] = Product{product.NewName, product.NewPrice, product.NewQuantity}
+			editedProduct = Product{product.NewName, product.NewPrice, product.NewQuantity}
 
-	res, err := db.Exec(sqlStatement, id)
-
-	if err != nil {
-		log.Fatalf("Unable to execute the query. %v", err)
+		}
 	}
 
-	rowsAffected, err := res.RowsAffected()
+	*reply = editedProduct
 
-	if err != nil {
-		log.Fatalf("Error while checking the affected rows. %v", err)
+	return nil
+}
+
+func (p *Products) DeleteProduct(product Product, reply *Product) error {
+
+	var deletedProduct Product
+
+	for i, v := range productSlice {
+		if v.Name == product.Name && v.Price == product.Price && v.Quantity == product.Quantity {
+			productSlice = append(productSlice[:i], productSlice[i+1:]...)
+			deletedProduct = product
+			break
+		}
 	}
 
-	fmt.Printf("Total rows/record affected %v", rowsAffected)
+	*reply = deletedProduct
 
-	return rowsAffected
+	return nil
 }
