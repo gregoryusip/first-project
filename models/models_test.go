db6c42921f1dca937fdd16d11ecd695b31cbe939
Gregoryus Imannuel Perdana <gregoryus@sirclo.com>
2021-03-18 17:28:27 +0700

    First-Project

--- models/models_test.go
+++ models/models_test.go
@@ -13,16 +13,16 @@ func TestCreateProduct(t *testing.T) {
 		Quantity int
 	}
 
-	var tests = []struct {
-		Name     string
-		Price    int
-		Quantity int
-	}{
-		{"Bola Basket", 250000, 400},
-		{"Meja Makan", 450000, 100},
-		{"TV LED", 1500000, 250},
-		{"Playstation 5", 8000000, 50},
-	}
+	// var tests = []struct {
+	// 	Name     string
+	// 	Price    int
+	// 	Quantity int
+	// }{
+	// 	{"Bola Basket", 250000, 400},
+	// 	{"Meja Makan", 450000, 100},
+	// 	{"TV LED", 1500000, 250},
+	// 	{"Playstation 5", 8000000, 50},
+	// }
 
 	// testingModel := Products{10, "Playstation 5", 8000000, 50}
 	testingModel := new(Products)
